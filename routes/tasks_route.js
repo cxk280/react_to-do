7f8255bf18fe055a03750324c7a7077187fa47e3
Christopher King <chrisking1@gmail.com>
2016-07-14 13:52:39 -0400

    updated this exercise

--- routes/tasks_route.js
+++ routes/tasks_route.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
 
+let taskData = [];
+
+// function addToTaskData(req,res,next) {
+//   taskData.push(req);
+//   next();
+// }
+
 router.get('/', function(req,res) {
   res.send('view task list')
 })
 
 router.post('/', function(req,res) {
-  res.send('posting to task list')
+  //pushes data into taskData
+  res.send(taskData)
 })
 
 router.get('/:id', function(req,res) {
