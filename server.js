cc31ab2eab543f65859fbccdcbc2f5995b32a3c3
Christopher King <chrisking1@gmail.com>
2016-07-14 12:17:16 -0400

    added bodyParser

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express           = require('express');
 const logger            = require('morgan');
 const path              = require('path');
+const bodyParser        = require('body-parser');
 const homeRoute         = require('./routes/home_route')
 const tasksRoute        = require('./routes/tasks_route')
 
@@ -10,6 +11,7 @@ const app               = express();
 const port              = process.argv[2] || process.env.PORT || 3009;
 
 app.use(logger('dev'));
+app.use( bodyParser.json());
 
 app.use('/', homeRoute);
 app.use('/tasks', tasksRoute);
