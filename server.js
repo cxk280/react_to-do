ab50634fdff35454c8859973a89bc93d3586a2ba
Christopher King <chrisking1@gmail.com>
2016-07-14 12:04:27 -0400

    made changes based on jason's walkthrough

--- server.js
+++ server.js
@@ -4,17 +4,24 @@ const express           = require('express');
 const logger            = require('morgan');
 const path              = require('path');
 
-const homeRoute         = require('./routes/home_route');
-const tasksRoute        = require('./routes/tasks_route');
-
 const app               = express();
-const port              = process.env.PORT || 3009;
+const port              = process.argv[2] || process.env.PORT || 3009;
 
 app.use(logger('dev'));
-
-app.use('/', homeRoute);
-app.use('/tasks', tasksRoute);
 
 app.listen(port, function() {
   console.log('Server is listening on ', port);
 })
+
+app.route('/tasks/:id')
+   .get((req, res)=> res.send(`view task ${req.params.id}`))
+   .put((req, res)=> res.send(`edited task ${req.params.id}`))
+   .delete((req, res)=> res.send(`deleted task ${req.params.id}`))
+
+app.route('/tasks')
+   .get((req, res)=> res.send('view task list'))
+   .post((req, res)=> res.send('posting to task list'))
+
+app.get('/', (req, res)=> res.send('homepage'))
+
+
