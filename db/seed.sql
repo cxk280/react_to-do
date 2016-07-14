39892e270d640a615d2bbeacabcaf302636cbb06
Christopher King <chrisking1@gmail.com>
2016-07-14 14:29:38 -0400

    completed exercise 3

---
+++ db/seed.sql
@@ -0,0 +1,14 @@
+INSERT INTO tasks (task_name, task_desc) VALUES(
+  'Walk the dog',
+  NULL
+);
+
+INSERT INTO tasks (task_name, task_desc) VALUES(
+  'Sleep',
+  'At least 8 hours'
+);
+
+INSERT INTO tasks (task_name, task_desc) VALUES(
+  'Homework',
+  'Finish all steps'
+);
